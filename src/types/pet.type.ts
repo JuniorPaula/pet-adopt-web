22af3a2f5aa9da155d6c3d4e40731ff5f168495e
Junior Paula <luke.junnior@icloud.com>
2025-02-19 19:18:33 -0300

    Implements pets page

---
+++ src/types/pet.type.ts
@@ -0,0 +1,11 @@
+export interface PetProps {
+  id: number
+  user_id: number
+  name: string
+  age: number
+  weight: number
+  size: string
+  color: string
+  images: string[]
+  available: boolean
+}
