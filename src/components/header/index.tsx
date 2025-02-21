8a74b29e3c001b438996fabb06650e657d5d8729
Junior Paula <luke.junnior@icloud.com>
2025-02-21 17:04:14 -0300

    Adjuste next image config Improviment header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useAuth } from "@/context/auth-context";
 import Link from "next/link";
+import { FaSignOutAlt } from "react-icons/fa";
 
 export function Header() {
+  const { user, logout } = useAuth();
+
   return (
     <header className="w-full h-20 bg-slate-100 text-black px-2 shadow-md">
       <div className="max-w-screen-xl mx-auto flex items-center justify-between h-20 sm:justify-between">
@@ -11,22 +17,35 @@ export function Header() {
         </nav>
 
         <div>
-          Ola, <span className="font-semibold">Usuário</span>
+          Ola, <span className="font-semibold text-gray-800">{ user?.first_name ?? "" }</span>
         </div>
 
         <div className="flex items-center justify-center space-x-4">
-          <Link href="/pets" className="text-lg font-semibold hover:text-blue-400 duration-200">
+          <Link href="/pets" className="text-lg hover:text-blue-700 duration-200">
             Pets
           </Link>
-          <Link href="/perfil" className="text-lg font-semibold hover:text-blue-400 duration-200">
-            Meu perfil
-          </Link>
-          <Link href="/login" className="text-lg font-semibold hover:text-blue-400 duration-200">
-            Login
-          </Link>
-          <Link href="/register" className="text-lg font-semibold hover:text-blue-400 duration-200">
-            Registrar
-          </Link>
+
+          { user ? (
+            <>
+              <Link href="/perfil" className="text-lg hover:text-blue-700 duration-200">
+                Meu perfil
+              </Link>
+
+              <button onClick={logout} className="text-xl text-red-500">
+                <FaSignOutAlt />
+              </button>
+            </>
+
+          ): (
+            <>
+              <Link href="/login" className="text-lg font-semibold hover:text-blue-900 duration-200">
+                Login
+              </Link>
+              <Link href="/register" className="text-lg font-semibold hover:text-blue-700 duration-200">
+                Registrar
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
