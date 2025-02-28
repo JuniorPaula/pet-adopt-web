a0845086c3343ecd7ce6c6e664de110ec4cf419b
Junior Paula <luke.junnior@icloud.com>
2025-02-28 09:01:09 -0300

    Fix layout to the container and footer

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,6 +1,6 @@
 export function Container({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="max-w-screen-xl mx-auto py-4 pb-36" style={{ height: "calc(100vh - 80px)" }}>
+    <div className="max-w-screen-xl mx-auto py-4 pb-36" style={{ minHeight: 'calc(100vh - 80px)' }}>
       { children }
     </div>
   )
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-slate-100 text-black text-center p-4">
+    <footer className="bg-slate-100 text-black text-center p-4 position-fixed bottom-0 w-full">
       <p className="text-sm">
         &copy; { new Date().getFullYear() } - PetAdopt - Todos os direitos reservados.
       </p>
